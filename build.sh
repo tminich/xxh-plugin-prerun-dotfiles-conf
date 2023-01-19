a3418912d88edaaf66a66a47f946fd13d1c052cc
Tobias Minich <tobias.minich@planet-itservices.com>
2023-01-19 13:42:03 +0100

    Update fpr config folder

--- build.sh
+++ build.sh
@@ -16,22 +16,33 @@ done
 cd $CDIR
 rm -rf $build_dir && mkdir -p $build_dir
 
-for f in *prerun.sh home
+configdir="$HOME/.xxh/.xxh/config/xxh-plugin-prerun-dotfiles-conf"
+homedir="$configdir/home"
+
+for f in *prerun.sh "$homedir"
 do
-    cp -R $CDIR/$f $build_dir/
+    cp -R $f $build_dir/
 done
 
-if [ -x "$(command -v pip)" ]; then
+pip_requirements_file="$configdir/pip-requirements.txt"
 
-  PYTHONUSERBASE=$build_dir/home/.local pip install --user -I -r pip-requirements.txt
-  
+pip_command="$(command -v pip)"
+
+if [ -z "$pip_command" ]; then
+  pip_command="$(command -v pip3)"
+fi
+
+if [ -x "$(command -v "$pip_command")" -a -f "$pip_requirements_file" ]; then
+
+  PYTHONUSERBASE=$build_dir/home/.local "$pip_command" install --user -I -r "$pip_requirements_file"
+
   # Fix python shebang
   pypath=`readlink -f $(which python)`
   if [ -d "$build_dir/home/.local/bin" ]; then
     echo 'Fix PyPi packages shebang'
     sed -i '1s|#!'$pypath'|#!/usr/bin/env python|' $build_dir/home/.local/bin/*
   fi
-  
+
 else
   echo 'Skip pip packages installation: pip not found.'
 fi
